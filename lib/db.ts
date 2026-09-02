14edcd54739a735b05e7513d7be92d90342284b0
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-09-02 22:16:17 +0700

    feat(cloud): register developer-cloud schema

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,9 @@
 import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
-import * as schema from "./schema";
+import * as legacySchema from "./schema";
+import * as cloudSchema from "./cloud/schema";
 
+const schema = { ...legacySchema, ...cloudSchema };
 const db = drizzle(sql, { schema, logger: true });
 
 export default db;
